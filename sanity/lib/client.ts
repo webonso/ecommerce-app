414ee283cfb3ad305c8d2777f50565d63796150a
technossau <cinonsookafor@gmail.com>
2023-08-07 12:42:03 +0100

    removed token from client

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,5 +7,4 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token: "skayiiEzlP7HVrtLlyum8HZOqYU5ujNSx1WcEk2LSUEFY4PENOskqjdeVJtrvf0t1FoBpJREFTrf1Z2Etpjo7tj7k1IAIdiR4pIKkxIZGinviIvQ59sUxcGOZOKg3uXkDgqirTYgXRBRXt5bO9WOdt8VqhGUwUIzjeOScy4ZZi2IR2NJfjNH"
 })
